670212c00cd34e1e129a269444844ec4303d3880
Scott Everett <burningcupidxxx@gmail.com>
2025-05-30 01:44:30 -0700

    changes for github pages deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
--- client/src/components/LacrimeHero.js
+++ client/src/components/LacrimeHero.js
@@ -5,16 +5,16 @@ const LacrimeHero = () => {
         <div id='lacrime-hero-cont'>
             <div id='lacrime-hero-left-cont'>
                 <div id='lacrime-logo-hero-cont'>
-                    <img id='lacrime-logo-hero' alt='lacrime logo' src='/images/lacrime-logo.png'></img>
-                    <img id='comin-soon-sticker' alt='coming soon graphic' src='/images/comin-soon.png'></img>
+                    <img id='lacrime-logo-hero' alt='lacrime logo' src={`${process.env.PUBLIC_URL}/images/lacrime-logo.png`}></img>
+                    <img id='comin-soon-sticker' alt='coming soon graphic' src={`${process.env.PUBLIC_URL}/images/comin-soon.png`}></img>
                 </div>
                 <div id='lacrime-hero-left-cont-inner'>
                     <p id='lacrime-hero-by'>by</p>
-                    <img id='whole-logo-hero' alt='whole company logo' src='/images/whole-logo.png'></img>
+                    <img id='whole-logo-hero' alt='whole company logo' src={`${process.env.PUBLIC_URL}/images/whole-logo.png`}></img>
                 </div>
             </div>
             <div id='lacrime-hero-right-cont'>
-                <img id='serum-bottle-hero' alt='lacrime serum bottle' src='/images/serum-bottle.png'></img>
+                <img id='serum-bottle-hero' alt='lacrime serum bottle' src={`${process.env.PUBLIC_URL}/images/serum-bottle.png`}></img>
             </div>
         </div>
     )
