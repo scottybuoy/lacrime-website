05110860dfd09ad82cb95e78b164f14901b2c032
Scott Everett <burningcupidxxx@gmail.com>
2025-05-30 23:19:25 -0700

    changes to homepage content, lighten navbar

--- client/src/components/LacrimeHero.js
+++ client/src/components/LacrimeHero.js
@@ -6,15 +6,11 @@ const LacrimeHero = () => {
             <div id='lacrime-hero-left-cont'>
                 <div id='lacrime-logo-hero-cont'>
                     <img id='lacrime-logo-hero' alt='lacrime logo' src={`${process.env.PUBLIC_URL}/images/lacrime-logo.png`}></img>
-                    <img id='comin-soon-sticker' alt='coming soon graphic' src={`${process.env.PUBLIC_URL}/images/comin-soon.png`}></img>
                 </div>
                 <div id='lacrime-hero-left-cont-inner'>
                     <p id='lacrime-hero-by'>by</p>
                     <img id='whole-logo-hero' alt='whole company logo' src={`${process.env.PUBLIC_URL}/images/whole-logo.png`}></img>
                 </div>
-            </div>
-            <div id='lacrime-hero-right-cont'>
-                <img id='serum-bottle-hero' alt='lacrime serum bottle' src={`${process.env.PUBLIC_URL}/images/serum-bottle.png`}></img>
             </div>
         </div>
     )
