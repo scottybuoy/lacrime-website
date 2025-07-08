0c033dfb3577b5d6b2dec29cddfca29057f357fc
Scott Everett <burningcupidxxx@gmail.com>
2025-07-08 12:31:49 -0700

    phase 2 v1 complete

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -9,29 +9,37 @@ const Nav = () => {
 
     const [activeLink, setActiveLink] = useState('');
 
-    // const updateActiveLink = (e) => {
-    //     let testState = '';
-    //     if (!e.target.children.length && e.target.tagName !== 'IMG') {
-    //         setActiveLink(e.target.textContent);
-    //     } else if (e.target.tagName === 'IMG') {
-    //         setActiveLink(e.target.nextElementSibling.textContent);
-    //     } else if (e.target.tagName === 'A') {
-    //         setActiveLink(e.target.children[0].children[1].textContent)
-    //     } else {
-    //         setActiveLink(e.target.children[1].textContent);
-    //     }
-    //     console.log(activeLink);
-    // }
+    const getTargetText = (e) => {
+        let targetText = '';
+        if (!e.target.children.length && e.target.tagName !== 'IMG') {
+            targetText = e.target.textContent;
+        } else if (e.target.tagName === 'IMG') {
+            targetText = e.target.nextElementSibling.textContent;
+        } else if (e.target.tagName === 'A') {
+            targetText = e.target.children[0].children[1].textContent;
+        } else {
+            targetText = e.target.children[1].textContent;
+        }
+        console.log('TARGET TEXT: ', targetText);
+        if (targetText === activeLink) {
+            return {fontWeight: '900'};
+        } else {
+            return {};
+        }
+    }
 
     const updateActiveLink = (e) => {
         console.log('URL: ', url)
         setActiveLink(url);
         console.log('STATE: ', activeLink);
-    }
+        getTargetText(e);
+    };
 
-    const activeLinkStyling = () => {
-
-    }
+    const activeLinkStyling = function(linkName) {
+        if (url === linkName) {
+            return {backgroundColor: 'rgb(211, 211, 211)'};
+        }
+    };
 
     return (
         <div className='nav-cont'>
@@ -42,29 +50,28 @@ const Nav = () => {
             </Link>
             <div
                 className='nav-items-cont'
-                // onClick={updateActiveLink}
                 onClick={updateActiveLink}
             >
                 <Link to={'/products'} className='link'>
-                    <div className='nav-link-cont'>
+                    <div className='nav-link-cont' style={activeLinkStyling('products')}>
                         <img className='nav-item-arrow' alt='arrow icon' src={`${process.env.PUBLIC_URL}/images/nav-item-arrow.png`}></img>
                         <p className='nav-link'>Products</p>
                     </div>
                 </Link>
                 <Link to={'/about'} className='link'>
-                    <div className='nav-link-cont'>
+                    <div className='nav-link-cont' style={activeLinkStyling('about')}>
                         <img className='nav-item-arrow' alt='arrow icon' src={`${process.env.PUBLIC_URL}/images/nav-item-arrow.png`}></img>
                         <p className='nav-link'>About</p>
                     </div>
                 </Link>
                 <Link to={'/our-science'} className='link'>
-                    <div className='nav-link-cont'>
+                    <div className='nav-link-cont' style={activeLinkStyling('our-science')}>
                         <img className='nav-item-arrow' alt='arrow icon' src={`${process.env.PUBLIC_URL}/images/nav-item-arrow.png`}></img>
                         <p className='nav-link'>Our Science</p>
                     </div>
                 </Link>
                 <Link to={'/contact'} className='link'>
-                    <div className='nav-link-cont'>
+                    <div className='nav-link-cont' style={activeLinkStyling('contact')}>
                         <img className='nav-item-arrow' alt='arrow icon' src={`${process.env.PUBLIC_URL}/images/nav-item-arrow.png`}></img>
                         <p className='nav-link'>Contact</p>
                     </div>
